152dc1e8e2fdbc9801a07274d57475431dc70174
İsmail ASLAN <1aslan1ismail@gmail.com>
2021-11-16 00:53:52 +0300

    create flight views

--- flightApp/views.py
+++ flightApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import HttpResponse
 from .models import Flight,Passenger,Reservation
 from .serializers import FlightSerializer,PassengerSerializer,ReservationSerializer
 from rest_framework import generics
+
 # from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 # from .permissions import IsAdminOrReadOnly, IsAddedByUserOrReadOnly
 
@@ -13,10 +14,9 @@ class FlightList(generics.ListCreateAPIView):
     serializer_class = FlightSerializer
     queryset = Flight.objects.all()
     # permission_classes = [IsAdminOrReadOnly]
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
 
 class FlightOperations(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = FlightSerializer
     queryset = Flight.objects.all()
+    lookup_field="id"
     # permission_classes = [IsAddedByUserOrReadOnly]
